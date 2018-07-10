6996951e20e81234cea22bd641dc12907b1be2be
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-07-10 22:45:28 +0300

    call object.pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-    raise 'exception'
+    @users.pry
+    # raise 'exception'
   end
 
   # GET /users/1
